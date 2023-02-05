3551c32ff888d49331ae2fdf93a8c76bfb23e11f
Rachel <101938842+RNN4830@users.noreply.github.com>
2023-02-05 06:50:11 -0700

    Update movies_update_and_delete.py

--- movies_update_and_delete.py
+++ movies_update_and_delete.py
@@ -8,7 +8,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host ="localhost",
     user ="root",
-    passwd = "H$8PjYFU",
+    passwd = "",
     database = "movies"
 )
 mycursor = mydb.cursor()
